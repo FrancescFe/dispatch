d9d585c21ea00014241329344f7ae5af9140f7cc
FrancescFe <fraferru@gmail.com>
2026-03-26 19:51:38 +0100

    feat(008): remove lombok from handler and service

--- src/main/java/org/francescfe/dispatch/service/DispatchService.java
+++ src/main/java/org/francescfe/dispatch/service/DispatchService.java
@@ -1,6 +1,5 @@
 package org.francescfe.dispatch.service;
 
-import lombok.RequiredArgsConstructor;
 import org.francescfe.dispatch.message.DispatchTracking;
 import org.francescfe.dispatch.message.OrderCreated;
 import org.francescfe.dispatch.message.OrderDispatched;
@@ -8,13 +7,16 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class DispatchService {
 
     private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
     private static final String DISPATCHED_STATUS = "DISPATCHED";
     private final KafkaTemplate<String, Object> kafkaProducer;
+
+    public DispatchService(KafkaTemplate<String, Object> kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
 
     public void process(OrderCreated orderCreated) throws Exception {
         OrderDispatched orderDispatched = new OrderDispatched(orderCreated.orderId());
