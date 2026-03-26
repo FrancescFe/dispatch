7061d8977fa96c1ab097995e5d0b737d1af07e98
Fran Fe <76252400+FrancescFe@users.noreply.github.com>
2026-03-26 15:08:09 +0100

    [005] Implement Producer (#5)
    * feat(005): add producer
    
    * feat(005): add producer
    
    * feat(005): remove warnings

--- src/test/java/org/francescfe/dispatch/service/DispatchServiceTest.java
+++ src/test/java/org/francescfe/dispatch/service/DispatchServiceTest.java
@@ -1,24 +1,58 @@
 package org.francescfe.dispatch.service;
 
 import org.francescfe.dispatch.message.OrderCreated;
+import org.francescfe.dispatch.message.OrderDispatched;
 import org.francescfe.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.concurrent.CompletableFuture;
 
 import static java.util.UUID.randomUUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class DispatchServiceTest {
 
     private DispatchService service;
+    @Mock
+    private KafkaTemplate<String, OrderDispatched> kafkaProducerMock;
 
     @BeforeEach
     void setUp() {
-        service = new DispatchService();
+        service = new DispatchService(kafkaProducerMock);
     }
 
     @Test
-    void process() {
+    void process_Success() throws Exception {
+        when(kafkaProducerMock.send(anyString(), any(OrderDispatched.class))).thenReturn(mock(CompletableFuture.class));
+
         OrderCreated testEvent = TestEventData.buildOrderCreated(randomUUID(), randomUUID().toString());
         service.process(testEvent);
+        verify(kafkaProducerMock, times(1)).send(eq("order.dispatched"), any(OrderDispatched.class));
+    }
+
+    @Test
+    public void process_ProducerThrowsException() {
+        OrderCreated testEvent = TestEventData.buildOrderCreated(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Producer failure")).when(kafkaProducerMock).send(eq("order.dispatched"), any(OrderDispatched.class));
+
+        Exception exception = assertThrows(RuntimeException.class, () -> service.process(testEvent));
+
+        verify(kafkaProducerMock, times(1)).send(eq("order.dispatched"), any(OrderDispatched.class));
+        assertEquals("Producer failure", exception.getMessage());
     }
 }
