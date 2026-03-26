5e76a439714b3f2dab2c231e335dd238fb9030c8
FrancescFe <fraferru@gmail.com>
2026-03-26 12:42:38 +0100

    feat(001): handle order created

---
+++ src/test/java/org/francescfe/dispatch/handler/OrderCreatedHandlerTest.java
@@ -0,0 +1,27 @@
+package org.francescfe.dispatch.handler;
+
+import org.francescfe.dispatch.service.DispatchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class OrderCreatedHandlerTest {
+
+    private OrderCreatedHandler handler;
+    private DispatchService dispatchServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
+    }
+
+    @Test
+    void listen() {
+        handler.listen("payload");
+        verify(dispatchServiceMock, times(1)).process("payload");
+    }
+}
---
+++ src/test/java/org/francescfe/dispatch/service/DispatchServiceTest.java
@@ -0,0 +1,19 @@
+package org.francescfe.dispatch.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DispatchServiceTest {
+
+    private DispatchService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new DispatchService();
+    }
+
+    @Test
+    void process() {
+        service.process("payload");
+    }
+}
