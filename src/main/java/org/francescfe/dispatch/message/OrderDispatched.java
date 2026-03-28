db61f8fdda1ff9c846a0679d6caa26aa22059589
Fran Fe <76252400+FrancescFe@users.noreply.github.com>
2026-03-28 18:52:39 +0100

    [012] Enable Multiple APP Instances Tracking (#12)

--- src/main/java/org/francescfe/dispatch/message/OrderDispatched.java
+++ src/main/java/org/francescfe/dispatch/message/OrderDispatched.java
@@ -2,5 +2,9 @@ package org.francescfe.dispatch.message;
 
 import java.util.UUID;
 
-public record OrderDispatched(UUID orderId) {
+public record OrderDispatched(
+        UUID orderId,
+        UUID processedById,
+        String notes
+) {
 }
--- src/main/java/org/francescfe/dispatch/service/DispatchService.java
+++ src/main/java/org/francescfe/dispatch/service/DispatchService.java
@@ -3,14 +3,22 @@ package org.francescfe.dispatch.service;
 import org.francescfe.dispatch.message.DispatchPreparing;
 import org.francescfe.dispatch.message.OrderCreated;
 import org.francescfe.dispatch.message.OrderDispatched;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
 
 @Service
 public class DispatchService {
 
+    private static final Logger log = LoggerFactory.getLogger(DispatchService.class);
     private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+    private static final UUID APPLICATION_ID = randomUUID();
     private final KafkaTemplate<String, Object> kafkaProducer;
 
     public DispatchService(KafkaTemplate<String, Object> kafkaProducer) {
@@ -18,10 +26,16 @@ public class DispatchService {
     }
 
     public void process(OrderCreated orderCreated) throws Exception {
-        OrderDispatched orderDispatched = new OrderDispatched(orderCreated.orderId());
-        kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
-
         DispatchPreparing dispatchPreparing = new DispatchPreparing(orderCreated.orderId());
         kafkaProducer.send(DISPATCH_TRACKING_TOPIC, dispatchPreparing).get();
+
+        OrderDispatched orderDispatched = new OrderDispatched(
+                orderCreated.orderId(),
+                APPLICATION_ID,
+                "Dispatched: " + orderCreated.item()
+        );
+        kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
+
+        log.info("Sent messages: orderId: {} - processedById: {}", orderCreated.orderId(), APPLICATION_ID);
     }
 }
